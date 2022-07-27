944126315cbbd4b48457fff3a8a3f143efef3211
Jacob Roger-Gordon <jrogergordon@gmail.com>
2022-07-26 23:58:58 -0400

    cursor, enemy, players all move properly, first flavor texts added

---
+++ README.md
@@ -0,0 +1,53 @@
+Jacob Irving Roger-Gordon;
+
+Project name: Mercenary Mayhem;
+Project type: Game;
+
+In Mercenary Mayhem, users will be able to:
+
+{Core features and functionalities:  
+1: Users will be able to move character pieces around a grid board;
+2: Users will be able to fight pieces controlled by an enemy AI to win the game;
+3: Users will be able to set the difficulty level of the AI;
+4: Users will be able to view animations of pieces fighting;
+5: Users will be able to view the information of character pieces;
+}
+{Additional features
+1.1: I will implement A* to inform AI movements as well as inform players of their characters available moves;
+1.2: Canvas animations to display character pieces;
+1.3: Animations and text will flash on screen when the game is won or lost;
+}
+
+{ In addition, my project will include:
+ 1: My ReadMe will have details about the project incuding instructions for operating the game and details about how the game works;
+ 2: Upon starting my game users will be presented by an instruction screen which will detail the rules of the game as well as how to interact with the game;
+}
+
+
+{Libraries and API's:
+To create this project I'll be Canvas and DOM;
+}
+
+
+{Implementation Timeline:
+Friday and Weekend {
+    Create a grid map using canvas and populate it with pieces. Be able to movie pieces around the grid;
+}
+
+Monday {
+    Allow users to move characters based on input, enable combat through user input;
+}
+
+Tuesday {
+    Generate AI;
+}
+
+Wednesday {
+    Create the rest of the UI;
+}
+
+Thursday Morning {
+    If nothing else needs fixing: Implement A*;
+}
+
+}
---
+++ src/grid.js
@@ -0,0 +1,386 @@
+class GridSystem {
+    constructor(matrix, player) {
+        this.matrix = matrix;
+        this.uiContext = this.#getContext(1000, 580, "gray");
+        this.outlineContext = this.#getContext(0, 0, "#444");
+        this.topContext = this.#getContext(0, 0, "#111", true);
+        this.cellSize = 25;
+        this.padding = 1;
+
+        this.player2 = { y: 4, x: 2, color: "#6EFF33", value: 6, health: 15,
+            atk: 3, spd: 5, moves: 6, startMoves: 6, name: "Jeff, the tepid"}
+        this.matrix[this.player2.x][this.player2.y] = 5;
+
+        this.player3 = { y: 2, x: 4, color: "#338642", value: 5, health: 40,
+            atk: 2, spd: 0, moves: 3, startMoves: 3, name: "Boyd, Stalwart Protector"}
+        this.matrix[this.player3.x][this.player3.y] = 6;
+
+        this.player = { y: 3, x: 3, color: "green", value: 2, health: 20, 
+            atk: 4, spd: 2, moves: 4, startMoves: 4, name: "Ike, Fearless Leader"}
+        this.matrix[this.player.x][this.player.y] = 2;
+
+
+        this.enemy = {x :23, y :11, color: "red", value: 4, health: 20,
+             atk: 4, spd: 3, moves: 4, name: "Petrine, the Firebrand", atk: 5, spd: 4}
+        this.matrix[this.enemy.y][this.enemy.x] = 4;
+
+        this.cursor = { y: 2, x: 2, color: "white" }
+        this.matrix[this.cursor.x][this.cursor.y] = 10
+        this.currPlayer = this.cursor;
+        this.cursor.value = 10;
+        this.allPlayers = [2, 5, 6]
+        this.allEnemies = [4]
+        this.stepOver = 1;
+        document.addEventListener("keydown", this.#movePlayer)
+        document.addEventListener("keydown", this.#selectPlayer)
+
+        this.water = [
+            "this tile is water",
+            "are you thirsty?",
+            "it's just water",
+            "splish splash",
+            "water is for winners",
+            "you cannot drink this",
+            "Thirst quenching",
+            "Water, it looks funny",
+            "Don't look at the water!",
+            "Do NOT drink"
+        ]
+        this.stone = [
+            "this tile is stone",
+            "be wary: stone pillar here",
+            "looks Grecian",
+            "Stone, almost crumbling",
+            "Stone Pillar, looks mighty",
+            "Stone Pillar, looks gaudy",
+            "Big Stone",
+            "Stone Pillar: don't ask",
+            "Don't touch the Stone",
+            "Do NOT eat"
+        ]
+        this.tile = [
+            "TILES ROCK MY WORLD",
+            "I LOVE TILES A LOT!",
+            "INTERIOR DESIGN!!!",
+            "TILE TILE TILE TILE",
+            "Just like HGTV- tileee!",
+            "Tiles are so cool!",
+            "I wish I was a tile",
+            "If only I were a tile",
+            "All I know is tile",
+            "Good place to sleep"
+        ]
+    }
+
+    #selectPlayer = ( { keyCode } ) => {
+        debugger;
+        if (keyCode === 32 && this.allPlayers.includes(this.stepOver)) {
+            this.matrix[this.currPlayer.y][this.currPlayer.x] = 10;
+            this.currPlayer = this.#whichPlayer(this.stepOver)
+            this.stepOver = this.currPlayer.value;
+        }
+    }
+
+    #isValidMove = (x, y, player) => {
+        if (this.currPlayer === this.cursor && this.matrix[this.currPlayer.y + y][this.currPlayer.x + x] !== 0){
+            return true;
+        } else if (this.matrix[this.currPlayer.y + y][this.currPlayer.x + x] === 1 || 
+            this.matrix[this.currPlayer.y + y][this.currPlayer.x + x] === 10) {
+            return true;
+        }
+        return false;
+    }
+
+    #movePlayer = ( { keyCode } ) => {
+        if(keyCode === 37) {
+            if (this.#isValidMove(-1, 0, this.currPlayer)) {
+                if (this.currPlayer.moves === this.currPlayer.startMoves) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = 10;
+                }
+                if(this.currPlayer === this.cursor) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = this.stepOver;
+                    this.stepOver = this.matrix[this.currPlayer.y][this.currPlayer.x - 1];
+                    this.matrix[this.currPlayer.y][this.currPlayer.x - 1] = this.currPlayer.value;
+                    this.currPlayer.x--;
+                } else {
+                    // if(this.matrix[this.currPlayer.y][this.currPlayer.x] !== 10){
+                        this.matrix[this.currPlayer.y][this.currPlayer.x] = 1;
+                    // }
+                    this.matrix[this.currPlayer.y][this.currPlayer.x - 1] = this.currPlayer.value;
+                    this.currPlayer.x--;
+                    this.currPlayer.moves--;
+                }
+            }
+        } else if(keyCode === 39) {
+            if (this.#isValidMove(1, 0, this.currPlayer)) {
+                if (this.currPlayer.moves === this.currPlayer.startMoves) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = 10;
+                }
+                if (this.currPlayer === this.cursor) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = this.stepOver;
+                    this.stepOver = this.matrix[this.currPlayer.y][this.currPlayer.x + 1];
+                    this.matrix[this.currPlayer.y][this.currPlayer.x + 1] = this.currPlayer.value;
+                    this.currPlayer.x++;
+                } else {
+                    // if (this.matrix[this.currPlayer.y][this.currPlayer.x] !== 10) {
+                        this.matrix[this.currPlayer.y][this.currPlayer.x] = 1;
+                    // }
+                    this.matrix[this.currPlayer.y][this.currPlayer.x + 1] = this.currPlayer.value;
+                    this.currPlayer.x++;
+                    this.currPlayer.moves--;
+                }
+            }
+        } else if (keyCode === 38) {
+            if (this.#isValidMove(0, -1, this.currPlayer)) {
+                if (this.currPlayer.moves === this.currPlayer.startMoves) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = 10;
+                }
+                if (this.currPlayer === this.cursor) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = this.stepOver;
+                    this.stepOver = this.matrix[this.currPlayer.y - 1][this.currPlayer.x];
+                    this.matrix[this.currPlayer.y - 1][this.currPlayer.x] = this.currPlayer.value;
+                    this.currPlayer.y--;
+                } else {
+                    // if (this.matrix[this.currPlayer.y][this.currPlayer.x] !== 10) {
+                        this.matrix[this.currPlayer.y][this.currPlayer.x] = 1;
+                    // }
+                    this.matrix[this.currPlayer.y - 1][this.currPlayer.x] = this.currPlayer.value;
+                    this.currPlayer.y--;
+                    this.currPlayer.moves--;
+                }
+            }
+        } else if (keyCode === 40) {
+            if (this.#isValidMove(0, 1, this.currPlayer)) {
+                if (this.currPlayer.moves === this.currPlayer.startMoves) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = 10;
+                }
+                if (this.currPlayer === this.cursor) {
+                    this.matrix[this.currPlayer.y][this.currPlayer.x] = this.stepOver;
+                    this.stepOver = this.matrix[this.currPlayer.y + 1][this.currPlayer.x];
+                    this.matrix[this.currPlayer.y + 1][this.currPlayer.x] = this.currPlayer.value;
+                    this.currPlayer.y++;
+                } else {
+                    // if (this.matrix[this.currPlayer.y][this.currPlayer.x] !== 10) {
+                        this.matrix[this.currPlayer.y][this.currPlayer.x] = 1;
+                    // }
+                    this.matrix[this.currPlayer.y + 1][this.currPlayer.x] = this.currPlayer.value;
+                    this.currPlayer.y++;
+                    this.currPlayer.moves--;
+                }
+            }
+        }
+        if(this.currPlayer.moves === 0){
+            if(1 === 1){
+                this.currPlayer.health--;
+                this.enemy.health--;
+            }
+            let holder = this.currPlayer
+            this.currPlayer = this.enemy;
+            this.#moveEnemy();
+            this.#moveEnemy();
+            this.#moveEnemy();
+            this.#moveEnemy();
+            holder.moves = holder.startMoves;
+            this.currPlayer = this.cursor;
+            this.stepOver = 1;
+        }
+        this.render();
+    }
+
+    #moveEnemy = () => {
+        let x = 0;
+        let y = 0;
+        const moves = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        let reach = 50;
+        let findX = this.player.x; 
+        let myX = this.enemy.x;
+        let specialX = -2;
+        let specialY = -2;
+
+        let findY = this.player.y;
+        let myY = this.enemy.y;
+        for(i = 0; i < moves.length; i++) {
+            let currX = myX + moves[i][0];
+            let currY = myY + moves[i][1];
+
+            let thisReach = Math.abs(currX - findX) + Math.abs(currY - findY);
+            if(thisReach < reach && this.#isValidMove(moves[i][0], moves[i][1], this.enemy)) {
+                    reach = thisReach;
+                x = moves[i][0];
+                y = moves[i][1];
+            }
+        }   
+        if((this.player.x === this.enemy.x && this.player.y === this.enemy.y + 1) ||
+            (this.player.x === this.enemy.x && this.player.y === this.enemy.y - 1) ||
+            (this.player.x === this.enemy.x + 1 && this.player.y === this.enemy.y) ||
+            (this.player.x === this.enemy.x - 1 && this.player.y === this.enemy.y)) {
+                x = 0;
+                y = 0;
+            }
+
+        if(this.#isValidMove(x, y, this.enemy)){
+            this.matrix[this.enemy.y][this.enemy.x] = 1;
+            this.matrix[this.enemy.y + y][this.enemy.x + x] = 4;
+            this.enemy.y += y;
+            this.enemy.x += x;
+        }
+        this.moved = true;
+    }
+
+    #getCenter(w, h) {
+        return {
+            x: window.innerWidth / 2 - w / 2 + "px",
+            y: window.innerHeight / 2 - h / 2 + "px"
+        };
+    }
+
+    #getContext(w, h, color = "#111", isTransparent = false) {
+        this.canvas = document.createElement("canvas")
+        this.context = this.canvas.getContext("2d")
+        this.width = this.canvas.width = w;
+        this.height = this.canvas.height = h;
+        this.canvas.style.position = "absolute";
+        this.canvas.style.background = color;
+        if(isTransparent) {
+            this.canvas.style.backgroundColor = "transparent";
+        }
+
+        const center = this.#getCenter(w, h);
+        this.canvas.style.marginLeft = center.x;
+        this.canvas.style.marginTop = center.y;
+        document.body.appendChild(this.canvas);
+        return this.context;
+    }
+
+    render() {
+        this.clearScreen();
+        const w = ((this.cellSize + this.padding) * this.matrix[0].length - (this.padding))
+        const h = ((this.cellSize + this.padding) * this.matrix.length - (this.padding))
+
+        this.outlineContext.canvas.width = w;
+        this.outlineContext.canvas.height = h;
+
+        const center = this.#getCenter(w, h);
+        this.outlineContext.canvas.style.marginLeft = center.x;
+        this.outlineContext.canvas.style.marginTop = center.y;
+
+        this.topContext.canvas.style.marginLeft = center.x;
+        this.topContext.canvas.style.marginTop = center.y;
+        for(let row = 0; row < this.matrix.length; row++){
+            for (let col = 0; col < this.matrix[row].length; col++){
+                let currentNode = [row, col];
+                let color = "orange";
+                if (this.matrix[row][col] === 10) {
+                color = this.cursor.color;
+                } else if (this.matrix[row][col] === 0){
+                    color = "black";
+                } else if(this.matrix[row][col] === 5) {
+                    color = this.player2.color;
+                } else if (this.matrix[row][col] === 6) {
+                    color = this.player3.color;
+                } else if (row < 9 && row > 5 && col < 17 && col > 9) {
+                    color = "blue";
+                    this.matrix[row][col] = 3; 
+                } else if (wallCheck(currentNode)) {
+                    color = "gray";
+                    this.matrix[row][col] = 7; 
+                } else if(this.matrix[row][col] === 2) {
+                    color = this.player.color;
+                } else if(this.matrix[row][col] === 4) {
+                    color = this.enemy.color;
+                }
+                this.outlineContext.fillStyle = color;
+                this.outlineContext.fillRect(col * (this.cellSize + this.padding),
+                row * (this.cellSize + this.padding),
+                this.cellSize, this.cellSize);
+
+            }
+        }
+        this.uiContext.clearRect(0, 0, 50000, 50000)
+        this.uiContext.font = "20px Courier";
+        this.uiContext.fillStyle = "white";
+        if (this.#whichPlayer(this.stepOver)) {
+            let showPlayer = this.#whichPlayer(this.stepOver)
+            this.uiContext.fillText("Player Health: " + showPlayer.health, 730, 30)
+            this.uiContext.fillText(showPlayer.name, 50, 550)
+            this.uiContext.fillText("Movements Left: " + showPlayer.moves, 50, 30)
+            this.uiContext.fillText("stats- " + "Atk: " + showPlayer.atk + " " + "Spd: " + showPlayer.spd, 710, 550)
+        } else if (this.allEnemies.includes(this.stepOver)) {
+            let showEnemy = this.#whichEnemy(this.stepOver)
+            this.uiContext.fillText("Enemy Health: " + showEnemy.health, 730, 30)
+            this.uiContext.fillText(showEnemy.name, 50, 550)
+            this.uiContext.fillText("Movements Left: " + showEnemy.moves, 50, 30)
+            this.uiContext.fillText("stats- " + "Atk: " + showEnemy.atk + " " + "Spd: " + showEnemy.spd, 670, 550)
+        } else if(this.stepOver === 3) {
+        // this.uiContext.clearRect(0, 0, this.width, this.height)
+        this.uiContext.fillText("A Water Tile", 730, 30)
+        this.uiContext.fillText("Players Cannot be Moved Here", 50, 550)
+        this.uiContext.fillText("A Water Tile", 50, 30)
+        this.uiContext.fillText(" " + this.water[Math.floor(Math.random() * 10)], 670, 550)
+        } else if(this.stepOver === 7) {
+        // this.uiContext.clearRect(0, 0, this.width, this.height)
+        this.uiContext.fillText("A Stone Column", 730, 30)
+        this.uiContext.fillText("Players Cannot be Moved Here", 50, 550)
+        this.uiContext.fillText("A Stone Column", 50, 30)
+        this.uiContext.fillText(" " + this.stone[Math.floor(Math.random() * 10)], 670, 550)
+        } else if (this.stepOver === 1) {
+            // this.uiContext.clearRect(0, 0, this.width, this.height)
+            this.uiContext.fillText("A Tile", 730, 30)
+            this.uiContext.fillText("Players move along these", 50, 550)
+            this.uiContext.fillText("A Tile", 50, 30)
+            this.uiContext.fillText(" " + this.tile[Math.floor(Math.random() * 10)], 670, 550)
+        }
+
+
+        function wallCheck(node) {
+            const river = [[10, 8], [9, 8], [10, 9], 
+            [4, 9], [5, 8], [4, 8], 
+            [10, 18], [10, 17], [9, 18],
+            [4, 18], [4, 17], [5, 18]]
+            for(i = 0; i < river.length; i++) {
+                if (node[0] === river[i][0] && node[1] === river[i][1]) return true;
+            }
+            return false;
+        }
+    }
+    clearScreen() {
+        this.uiContext.fillStyle = "black";
+        this.uiContext.fillRect(0,0, this.canvas.width, this.canvas.height);
+    }
+
+    #whichPlayer(n) {
+        if (n === 2) {
+            return this.player;
+        } else if (n === 5) {
+            return this.player3;
+        } else if (n === 6) {
+            return this.player2;
+        }
+        return false;
+    }
+    #whichEnemy(n) {
+        if (n === 4) {
+            return this.enemy;
+        }
+    }
+}
+
+
+function createMatrix(x, y) { 
+    let arr2d = new Array(x)
+    for(i = 0; i < x; i++) {
+        arr2d[i] = new Array(y);
+        for(j = 0; j < y; j++) {
+            if(i === 0 || j === 0 || i === x - 1 || j === y - 1) {
+                arr2d[i][j] = 0
+            } else{ 
+                arr2d[i][j] = 1
+            }
+        }
+    }
+    return arr2d;
+}
+const matrix = createMatrix(15, 27);
+const grid = new GridSystem(matrix);    
+
+grid.render();
